9a583fc70d8713b0235e9d9ff78a222fdc49c465
jespanap1 <jespanap@eafit.edu.co>
2023-09-26 23:53:02 -0500

    CRUD TERMINADO

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,59 +3,113 @@ const { Canciones, Albumes, Artistas, Generos } = require("../database/models");
 const controller = {
   getAdmin: async (req, res) => {
     try {
+      const generosList = await Generos.findAll();
+      const artistasList = await Artistas.findAll();
+      const albumesList = await Albumes.findAll();
+
       const canciones = await Canciones.findAll({
         raw: true,
       });
 
-      res.render("admin", { canciones });
+      res.render("admin", { canciones, generosList, artistasList, albumesList });
     } catch (error) {
-      res.render("admin", { canciones: [] });
+      res.render("admin", { canciones: [], generosList: [], artistasList: [], albumesList: [] });
       console.log(error);
     }
   },
 
   getAdminCreate: async (req, res) => {
     //res.sendFile(path.join(__dirname, '../views/admin-create.html'));
-    res.render("admin-create");
+    try {
+      const generosList = await Generos.findAll();
+      const artistasList = await Artistas.findAll();
+      const albumesList = await Albumes.findAll();
+      res.render('admin-create', { title: 'Crear Nueva Canción', generos: generosList, artistas: artistasList, albumes: albumesList });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ status: 'error', message: 'Error al obtener la vista de creación' });
+    }
+    //res.render("admin-create");
   },
 
   postCancion: async (req, res) => {
-    const { titulo, duracion, genero, album, artista, apellido_artista } = req.body;
-    console.log(req.body)
+    const nuevaCancion = {
+      titulo: req.body.titulo,
+      duracion: req.body.duracion,
+      genero_id: req.body.genero,
+      album_id: req.body.album,
+      artista_id: req.body.artista,
+    };
+
     try {
-        // Verificar si el artista existe por su nombre y apellido
-        let artistaExistente = await Artistas.findOne({ where: { nombre: artista, apellido: apellido_artista } });
-
-        // Si el artista no existe, créalo
-        if (!artistaExistente) {
-            artistaExistente = await Artistas.create({ nombre: artista, apellido: apellido_artista });
-        }
-
-        // Verificar si el álbum existe por su nombre
-        let albumExistente = await Albumes.findOne({ where: { nombre: album } });
-
-        // Si el álbum no existe, créalo con una duración inicial en segundos
-        if (!albumExistente) {
-            albumExistente = await Albumes.create({ nombre: album, duracion: 0 });
-        }
-
-        // Luego, crea la canción asociándola al álbum existente (o recién creado)
-        const nuevaCancion = await Canciones.create({
-            titulo,
-            duracion, // Almacena la duración tal como la ingresó el usuario (en segundos)
-            GeneroId: genero, // Asume que GeneroId es el campo de clave foránea en la tabla Canciones
-            AlbumId: albumExistente.id,
-            ArtistaId: artistaExistente.id,
-        });
-
-        // Envía una respuesta de éxito
-        res.status(201).send("Canción creada con éxito");
+      const datos = await Canciones.create(nuevaCancion);
+      console.log(datos);
     } catch (error) {
-        console.error(error);
-        // Manejar el error y enviar una respuesta de error si es necesario
-        res.status(500).send("Error al crear la canción");
+      console.log(error);
+    }
+
+    res.send('Canción creada con exito');
+  },
+
+  getAdminEdit: async (req, res) => {
+    try {
+      const cancion = await Canciones.findByPk(req.params.id);
+
+      if (!cancion) {
+        return res.status(404).json({ message: 'Canción no encontrada' });
+      }
+
+      // Obtén otras listas necesarias, como generosList, artistasList, albumesList
+      const generosList = await Generos.findAll();
+      const artistasList = await Artistas.findAll();
+      const albumesList = await Albumes.findAll();
+
+      res.render('admin-edit', {
+        title: 'Editar Canción',
+        cancion,
+        generos: generosList,
+        artistas: artistasList,
+        albumes: albumesList,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ status: 'error', message: 'Error al cargar la canción para editar' });
     }
   },
+
+  putCancion: async (req, res) => {
+    console.log(req.body);
+
+    const newValues = req.body;
+
+    try {
+      await Canciones.update(newValues, {
+        where: {
+          id: req.body.id
+        }
+      });
+
+      res.redirect('/');
+    } catch (error) {
+      res.send('No se pudo actualizar!')
+      console.log(error);
+    }
+  },
+
+  deleteCancion: async (req, res) => {
+    const { id } = req.params;
+    const deleteRowCount = await Canciones.destroy({
+      where: {
+        id: id
+      }
+    })
+    res.json({
+      message: 'Cancion eliminada correctamente',
+      count: deleteRowCount
+    })
+
+  }
+
 
 };
 
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,11 +6,20 @@ const adminController = require('../controllers/adminController');
 // @GET /  "admin"  en realidad esta recibiendo el index o home
 router.get('/', adminController.getAdmin);
 
-// @POST /admin
+// @POST
 router.post('/', adminController.postCancion);
 
-// @GET /crear
+// @GET /crear 
 router.get('/crear', adminController.getAdminCreate); 
+
+// @GET /editar-cancion/:id -- Aqui renderizo el editar
+router.get('/editar-cancion/:id', adminController.getAdminEdit); 
+
+// @PUT /editar   -- Aqui se hace la magia del editar 
+router.put('/', adminController.putCancion);
+
+// @DELETE 
+router.delete('/canciones/:id', adminController.deleteCancion);
 
 
 
